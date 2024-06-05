83c2767032783c84965ef19dd02158def5b7a391
Utsav_Prajapati_BSI <utsav.prajapati@botspotinfoware.com>
2024-06-05 15:39:36 +0530

    minor changes

--- learning_js/projects/22_password_strength/password_strength.js
+++ learning_js/projects/22_password_strength/password_strength.js
@@ -3,24 +3,39 @@ let msg = document.getElementById("message")
 let strength = document.getElementById("strength")
 
 password.addEventListener("input", ()=>{
-    console.log(password.value.length);
     if(password.value.length > 0){
         msg.style.display = "block"
+        if(password.value.length < 4 && password.value.length >0){
+            strength.innerHTML = "Weak";
+            password.style.borderColor = "#ff5925";
+            msg.style.color = "#ff5925";
+        } else if(password.value.length >= 4 && (password.value.length < 8)){
+            strength.innerHTML = "Medium";
+            password.style.borderColor = "yellow";
+            msg.style.color = "yellow";
+        } else if(password.value.length >= 8){
+            strength.innerHTML = "Strong";
+            password.style.borderColor = "green";
+            msg.style.color = "green";
+        }
     }
     else{
-        msg.style.display = "none"
-    }
-    if(password.value.length < 4){
-        strength.innerHTML = "Weak";
-        password.style.borderColor = "#ff5925";
-        msg.style.color = "#ff5925";
-    } else if(password.value.length >= 4 && (password.value.length < 8)){
-        strength.innerHTML = "Medium";
-        password.style.borderColor = "yellow";
-        msg.style.color = "yellow";
-    } else if(password.value.length >= 8){
-        strength.innerHTML = "Strong";
-        password.style.borderColor = "green";
-        msg.style.color = "green";
+        password.style.borderColor = "#fff";
+        msg.style.display = "none";
     }
 })
+
+// for password valid or invalid
+// const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,}$/
+// if(regex.test(password.value)) {
+//     console.log('valid password!')
+//     valid_invalid_message.innerHTML = "Valid Password";
+//     password.style.borderColor = "green";
+//     valid_invalid_strength.style.color = "green";
+//   }
+//   else {
+//     console.log("Invalid Password");
+//     valid_invalid_message.innerHTML = "Invalid Password";
+//     password.style.borderColor = "#ff5925";
+//     valid_invalid_strength.style.color = "#ff5925";
+//   }
